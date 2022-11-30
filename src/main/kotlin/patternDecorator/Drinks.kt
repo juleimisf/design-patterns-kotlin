13b701c6ca0f91ab5c3f3b880894c3cf5c15effd
juleimisf <juleimis1314@gmail.com>
2022-11-29 23:12:13 -0300

    finish decorator

--- src/main/kotlin/patternDecorator/Drinks.kt
+++ src/main/kotlin/patternDecorator/Drinks.kt
@@ -1,38 +1,41 @@
 package patternDecorator
 
-abstract class Drink(val desc: String = "Non drink name") {
-    abstract fun cost(): Double
+interface Drink {
+    fun cost(): Double
+    fun getDescription(): String
 }
 
-class Coffee : Drink("Black coffee") {
-    override fun cost() = 2.50
+class DarkRoast : Drink {
+    override fun cost() = 1.0
+    override fun getDescription() = "Dark Roast"
+}
+
+class Coffee : Drink {
+    override fun cost() = 1.1
+    override fun getDescription() = "Black coffee"
 
 }
 
-class NonCaffeine : Drink("Non caffeinated coffe") {
-    override fun cost() = 2.5
+class NonCaffeine : Drink {
+    override fun cost() = 2.0
+    override fun getDescription() = "Non Caffeine"
 }
 
-abstract class AdditiveDecorator : Drink() {
-    abstract fun description(): String
-}
+abstract class AdditiveDecorator : Drink
 
-class MilkDecorator(val drink: Drink) : AdditiveDecorator() {
-    override fun description() = "${drink.desc} + Milk"
-
-    override fun cost() = 0.5
+class MilkDecorator(var drink: Drink) : AdditiveDecorator() {
+    override fun cost() = drink.cost() + 1.0
+    override fun getDescription() = drink.getDescription() + " + Milk"
 
     companion object {
         fun withDrink(newDrink: Drink) = MilkDecorator(newDrink)
     }
 }
 
-class CinnamonDecorator(val drink: Drink) : AdditiveDecorator() {
+class CinnamonDecorator(var drink: Drink) : AdditiveDecorator() {
     override fun cost() = drink.cost() + 1.0
-    override fun description() = "${drink.desc} + Cinnamon"
-
+    override fun getDescription() = drink.getDescription() + " + Cinnamon"
     companion object {
         fun withDrink(newDrink: Drink) = CinnamonDecorator(newDrink)
     }
-
 }
--- src/main/kotlin/patternDecorator/Main.kt
+++ src/main/kotlin/patternDecorator/Main.kt
@@ -1,8 +1,11 @@
 package patternDecorator
 
 fun main(args: Array<String>) {
-    val coffee = Coffee()
-    val additional = CinnamonDecorator.withDrink(coffee)
-    println("Ordenaste: ${additional.description()}\nCosto: ${additional.cost()}$")
+    var coffee: Drink = Coffee()
+    coffee = CinnamonDecorator.withDrink(coffee)
+    coffee = CinnamonDecorator.withDrink(coffee)
+    coffee = MilkDecorator.withDrink(coffee)
+
+    println("Ordenaste: ${coffee.getDescription()}\nCosto: ${coffee.cost()}$")
 
 }
